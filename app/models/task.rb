2f1e99ae6c225b2f7c8021cd8ac041759d1b912d
JorgeOmar <ulloa13@hotmail.com>
2016-04-13 22:11:37 -0500

    paso9

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ActiveRecord::Base
   validates :title, presence: true
   belongs_to :list
+  scope :incomplete, -> {where("completed = 'f'")}
+  scope :complete, -> {where("completed = 't'")}
+  #def self.incomplete
+  #where ("complete = 'f'")
+  #end
+
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,10 +4,19 @@ class TaskTest < ActiveSupport::TestCase
   test 'task without name must be invalid' do
   	task = Task.new title: nil
   assert_not task.valid?
-end
-test 'task has one list' do
+  end
+
+  test 'task has one list' do
 	assert_not_nil tasks(:one).list
-end
+  end
 
-end
+  test 'show incomplete tasks' do
+	incomplete_tasks = Task.incomplete
+	assert_includes incomplete_tasks, tasks(:one)
+  end
 
+  test 'show complete tasks' do
+    complete_tasks = Task.complete
+    assert_includes complete_tasks, tasks(:two)
+  end
+  end
